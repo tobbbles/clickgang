ff56080d8c0721b5225925a638debe686770c188
Michael Craddock <michael@moblimic.com>
2019-03-22 13:39:24 +0000

    indenting

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
 func main() {
     log.SetFlags(log.LstdFlags|log.Lshortfile)
 
-
-   go func (){
+    go func (){
         srv, err := server.New("localhost:3117")
         if err != nil {
             log.Fatal(err)
         }
         log.Fatal(srv.Start())
-	}()
+    }()
 
     go func (){
         srv2, err := server.NewWeb("localhost:4040")
